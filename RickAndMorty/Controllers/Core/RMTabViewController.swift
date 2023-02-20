5d8c8b0018bb319bcc0f3bba91b4ad1fedadc1c1
Taehwan Kim <obtusa07@gmail.com>
2023-02-20 22:11:44 +0900

    :sparkles: set tabVarVC

--- RickAndMorty/Controllers/Core/RMTabViewController.swift
+++ RickAndMorty/Controllers/Core/RMTabViewController.swift
@@ -7,23 +7,41 @@
 
 import UIKit
 
-class RMTabViewController: UITabBarController {
+final class RMTabViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        setupTabs()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupTabs() {
+        let charactersVC =  RMCharacterViewController()
+        let locationsVC = RMLocationViewController()
+        let episodesVC = RMEpisodeViewController()
+        let settingsVC = RMSettingsViewController()
+        
+        charactersVC.navigationItem.largeTitleDisplayMode = .automatic
+        locationsVC.navigationItem.largeTitleDisplayMode = .automatic
+        episodesVC.navigationItem.largeTitleDisplayMode = .automatic
+        settingsVC.navigationItem.largeTitleDisplayMode = .automatic
+        
+        let nav1 = UINavigationController(rootViewController: charactersVC)
+        let nav2 = UINavigationController(rootViewController: locationsVC)
+        let nav3 = UINavigationController(rootViewController: episodesVC)
+        let nav4 =  UINavigationController(rootViewController: settingsVC)
+        
+        nav1.tabBarItem = UITabBarItem(title: "Characters", image: UIImage(systemName: "person"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Locations", image: UIImage(systemName: "globe"), tag: 2)
+        nav3.tabBarItem = UITabBarItem(title: "Episodes", image: UIImage(systemName: "tv"), tag: 3)
+        nav4.tabBarItem = UITabBarItem(title: "Settings", image: UIImage(systemName: "gear"), tag: 4)
+        
+        
+        for nav in [nav1, nav2, nav3, nav4] {
+            nav.navigationBar.prefersLargeTitles = true
+        }
+                    
+        
+        setViewControllers([nav1, nav2, nav3, nav4], animated: true)
     }
-    */
 
 }
